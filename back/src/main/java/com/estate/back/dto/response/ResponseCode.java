08b513310e293144e362b70191400fd44157cea6
jojaehyeok99 <choco5366@naver.com>
2024-04-24 15:08:50 +0900

    2024 - 04 - 24 프로젝트 : Response 공통 형태 작업(code, message)

---
+++ back/src/main/java/com/estate/back/dto/response/ResponseCode.java
@@ -0,0 +1,22 @@
+package com.estate.back.dto.response;
+
+// Response의 공통된 code 값
+public interface ResponseCode {
+  
+  // 200
+  String SUCCESS = "SU";
+
+  // 400
+  String VALIDATION_FAILED = "VF";
+  String DUPLICATED_ID = "DI";
+  String DUPLICATED_EMAIL = "DE";
+
+  // 401
+  String SIGN_IN_FAILED = "SF";
+  String AUTHENTICATION_FAILED = "AF";
+
+  // 500
+  String TOKEN_CREATION_FAILED = "TF";
+  String DATABASE_ERROR = "DBE";
+  String MAIL_SEND_FAILED = "MF";
+}
---
+++ back/src/main/java/com/estate/back/dto/response/ResponseMessage.java
@@ -0,0 +1,22 @@
+package com.estate.back.dto.response;
+
+// Response의 공통된 message 값
+public interface ResponseMessage {
+
+  // 200
+  String SUCCESS = "Success.";
+
+  // 400
+  String VALIDATION_FAILED = "Validation Failed.";
+  String DUPLICATED_ID = "Duplicated Id.";
+  String DUPLICATED_EMAIL = "Duplicated Email.";
+
+  // 401
+  String SIGN_IN_FAILED = "Sign in failed.";
+  String AUTHENTICATION_FAILED = "Authorization Failed.";
+
+  // 500
+  String TOKEN_CREATION_FAILED = "Token creation Failed.";
+  String DATABASE_ERROR = "Database Error.";
+  String MAIL_SEND_FAILED = "Mail send Failed.";
+}
