8006e7773c86446d7aafa8022314efb80efc877c
jojaehyeok99 <choco5366@naver.com>
2024-04-26 09:52:52 +0900

    2024 - 04 - 26 프로젝트 : 프론트 패키지 설치 및 프로젝트 초기화 순서 작성

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,3 +6,12 @@ function App() {
 }
 
 export default App;
+
+// - authentication(로그인, 회원가입)
+// - service
+//   - locale(지역 평균)
+//   - ratio(비율 계산)
+//   - qna(QnA 리스트)
+//     - :boardNumber(QnA 상세보기)
+//     - write(QnA 작성)
+//     - update/:boardNumber
