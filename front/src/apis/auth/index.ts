639e4cd8e9225716479df41489aa80667ea145e6
jojaehyeok99 <choco5366@naver.com>
2024-04-26 12:47:46 +0900

    2024 - 04 - 26 프로젝트 : 공통 Request, Request Error 처리 함수 생성, 각 API 함수 생성

--- front/src/apis/auth/index.ts
+++ front/src/apis/auth/index.ts
@@ -1,14 +1,46 @@
 import axios from "axios";
-import { SignInRequestDto } from "./dto/request";
-import { SIGN_IN_REQUEST_URL } from "src/constant";
+import { EmailAuthCheckRequestDto, EmailAuthRequestDto, IdCheckRequestDto, SignInRequestDto, SignUpRequestDto } from "./dto/request";
+import { EMAIL_AUTH_CHECK_REQUEST_URL, EMAIL_AUTH_REQUEST_URL, ID_CHECK_REQUEST_URL, SIGN_IN_REQUEST_URL, SIGN_UP_REQUEST_URL } from "src/constant";
 import { SignInResponseDto } from "./dto/response";
+import ResponseDto from "../response.dto";
+import { requestErrorHandler, requestHandler } from "..";
 
 // function : 로그인 API 함수
 export const SignInRequest = async (requestBody : SignInRequestDto) => {
   const result = await axios.post(SIGN_IN_REQUEST_URL, requestBody)
-    .then(response => {
-      const responseBody = response.data as SignInResponseDto;
-      return responseBody;
-    })
-    .catch(error => {})
-}
+    .then(requestHandler<SignInResponseDto>)
+    .catch(requestErrorHandler);
+  return result;
+};
+
+// function : 아이디 중복 확인 API 함수
+export const IdCheckRequest = async (requestBody : IdCheckRequestDto) => {
+  const result = await axios.post(ID_CHECK_REQUEST_URL, requestBody)
+    .then(requestHandler<ResponseDto>)
+    .catch(requestErrorHandler);
+  return result;
+};
+
+// function : 이메일 인증 API 함수
+export const EmailAuthRequest = async(requestBody : EmailAuthRequestDto) => {
+  const result = await axios.post(EMAIL_AUTH_REQUEST_URL, requestBody)
+    .then(requestHandler<ResponseDto>)
+    .catch(requestErrorHandler);
+  return result;
+};
+
+// function : 이메일 인증 확인 API 함수
+export const EmailAuthCheckRequest = async(requestBody : EmailAuthCheckRequestDto) => {
+  const result = await axios.post(EMAIL_AUTH_CHECK_REQUEST_URL, requestBody)
+    .then(requestHandler<ResponseDto>)
+    .catch(requestErrorHandler);
+  return result;
+};
+
+// function : 회원가입 API 함수
+export const SignUpRequest = async(requestBody : SignUpRequestDto) => {
+  const result = await axios.post(SIGN_UP_REQUEST_URL, requestBody)
+    .then(requestHandler<ResponseDto>)
+    .catch(requestErrorHandler);
+  return result;
+};
