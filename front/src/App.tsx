e2e415be714ee0e43eb19c72e6de0c2c1898760b
jojaehyeok99 <choco5366@naver.com>
2024-04-26 11:07:11 +0900

    2024 - 04 - 26 프로젝트 : 공통 상수 url- constant index.ts 지정, root 경로의 index.tsx browserRouter 등록, App.tsx 네비게이션 지정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import { AUTH_PATH, LOCAL_PATH, QNA_DETAIL_PATH, QNA_PATH, QNA_UPDATE_PATH, QNA_WRITE_PATH, RATIO_PATH, SERVICE_PATH } from './constant';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path={AUTH_PATH} element = {<></>}/>
+      <Route path={SERVICE_PATH} element = {<></>}>
+        <Route path={LOCAL_PATH} element = {<></>}/>
+        <Route path={RATIO_PATH} element = {<></>}/>
+        <Route path={QNA_PATH} >
+          <Route index element = {<></>}/>
+          <Route path={QNA_WRITE_PATH} element = {<></>}/>
+          <Route path={QNA_DETAIL_PATH} element = {<></>}/>
+          <Route path={QNA_UPDATE_PATH} element = {<></>}/>
+        </Route>
+      </Route>
+      <Route path='*' element={<></>}/>
+    </Routes>
+  );
 }
 
 export default App;
 
 // - authentication(로그인, 회원가입)
 // - service
-//   - locale(지역 평균)
+//   - local(지역 평균)
 //   - ratio(비율 계산)
 //   - qna(QnA 리스트)
 //     - :boardNumber(QnA 상세보기)
