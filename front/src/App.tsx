ed88a55225d9dcdd37b73ea5baa74f36a2388110
jojaehyeok99 <choco5366@naver.com>
2024-04-29 16:58:17 +0900

    2024 - 04 - 29 프로젝트 : 프론트  카카오, 네이버 oauth 인증 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { Route, Routes, useNavigate } from 'react-router';
 import { AUTH_ABSOLUTE_PATH, AUTH_PATH, LOCAL_ABSOLUTE_PATH, LOCAL_PATH, QNA_DETAIL_PATH, QNA_PATH, QNA_UPDATE_PATH, QNA_WRITE_PATH, RATIO_PATH, SERVICE_PATH } from './constant';
-import Authentication from './views/Authentication';
+import Authentication, { Sns } from './views/Authentication';
 import ServiceContainer from './layouts/ServiceContainer';
 import Local from './views/service/Local';
 import Ratio from './views/service/Ratio';
@@ -40,6 +40,7 @@ function App() {
   return (
     <Routes>
       <Route index element = {<Index />} />
+      <Route path={'/sns/:accessToken/:expires'} element = {<Sns/>} />
       <Route path={AUTH_PATH} element = {<Authentication />}/>
       <Route path={SERVICE_PATH} element = {<ServiceContainer/>}>
         <Route path={LOCAL_PATH} element = {<Local />}/>
