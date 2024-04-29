db6db25ceb67a8e2f97744e24cca210cdf94c3bf
jojaehyeok99 <choco5366@naver.com>
2024-04-29 11:15:38 +0900

    2024 - 04 - 29 프로젝트 : 프론트 이메일 인증확인, 회원가입 api 구현

--- front/src/views/Authentication/index.tsx
+++ front/src/views/Authentication/index.tsx
@@ -3,8 +3,8 @@ import './style.css';
 import SignInBackground from 'src/assets/image/sign-in-background.png';
 import SignUpBackground from 'src/assets/image/sign-up-background.png';
 import InputBox from 'src/components/Inputbox';
-import { EmailAuthRequestDto, IdCheckRequestDto } from 'src/apis/auth/dto/request';
-import { EmailAuthRequest, IdCheckRequest } from 'src/apis/auth';
+import { EmailAuthCheckRequestDto, EmailAuthRequestDto, IdCheckRequestDto, SignUpRequestDto } from 'src/apis/auth/dto/request';
+import { EmailAuthCheckRequest, EmailAuthRequest, IdCheckRequest, SignUpRequest } from 'src/apis/auth';
 import ResponseDto from 'src/apis/response.dto';
 
 //                    type                    //
@@ -163,6 +163,43 @@ function SignUp ({onLinkClickHandler} : Props) {
     setEmailError(emailError);
   };
 
+  const emailAuthCheckResponse = (result : ResponseDto | null) => {
+
+    const authNumberMessage = 
+      !result ? '서버에 문제가 있습니다.':
+      result.code === 'VF' ? '인증번호를 입력해주세요.' :
+      result.code === 'AF' ? '인증번호가 일치하지 않습니다.' :
+      result.code === 'DBE' ? '서버에 문제가 있습니다.' :
+      result.code === 'SU' ? '인증번호가 확인되었습니다.' : '';
+    
+      const authNumberCheck = result !== null && (result.code === 'SU');
+      const authNumberError = !authNumberCheck;
+
+      setAuthNumberMessage(authNumberMessage);
+      setAuthNumberCheck(authNumberCheck);
+      setAuthNumberError(authNumberError);
+  };
+
+  const signUpResponse = (result : ResponseDto | null) => {
+
+    const message = 
+    !result ? '서버에 문제가 있습니다.':
+    result.code === 'VF' ? '입력 형식이 맞지 않습니다.' :
+    result.code === 'DI' ? '이미 사용중인 아이디입니다.' :
+    result.code === 'DE' ? '중복된 이메일입니다.' :
+    result.code === 'AF' ? '인증번호가 일치하지 않습니다.' :
+    result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
+
+    const isSuccess = result && result.code === 'SU';
+    if (!isSuccess){
+      alert(message);
+      return;
+    }
+
+    onLinkClickHandler();
+
+  };
+
   //                    event handler                    //
   const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
@@ -239,18 +276,21 @@ function SignUp ({onLinkClickHandler} : Props) {
 
   const onAuthNumberButtonClickHandler = () => {
     if(!authNumberButtonStatus) return;
+    if(!authNumber) return;
     
-    const authNumberCheck = authNumber === '1234';
-    setAuthNumberCheck(authNumberCheck);
-    setAuthNumberError(!authNumberCheck);
-
-    const authNumberMessage = authNumberCheck ? '인증번호가 확인되었습니다.' : '인증번호가 일치하지 않습니다.';
-    setAuthNumberMessage(authNumberMessage);
+    const requestBody : EmailAuthCheckRequestDto = {userEmail : email, authNumber};
+    EmailAuthCheckRequest(requestBody).then(emailAuthCheckResponse);
   };
 
   const onSignUpButtonClickHandler = () => {
     if (!isSignUpActive) return;
-    alert('회원가입');
+    if (!id || !password || !email || !authNumber) {
+      alert('모든 내용을 입력해주세요.');
+      return;
+    };
+
+    const requestBody : SignUpRequestDto = {userId : id, userPassword : password, userEmail : email, authNumber};
+    SignUpRequest(requestBody).then(signUpResponse);
   };
 
 
