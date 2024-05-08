da951692ad013a565230073ac9a9e009b03f7c45
jojaehyeok99 <choco5366@naver.com>
2024-05-08 17:35:02 +0900

    2024 - 05 - 08 프로젝트 : 프론트 - 로그인 시 enter키 사용

--- front/src/components/Inputbox/index.tsx
+++ front/src/components/Inputbox/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, KeyboardEvent } from 'react'
 import './style.css';
 
 export interface InputBoxProps{
@@ -12,9 +12,10 @@ export interface InputBoxProps{
   onButtonClickHandler?: () => void;
   message? : string;
   error? : boolean;
+  onKeydownHandler? : (event: KeyboardEvent<HTMLInputElement>) => void;
 }
 
-export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle, buttonStatus, onButtonClickHandler, message, error} : InputBoxProps) {
+export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle, buttonStatus, onButtonClickHandler, message, error, onKeydownHandler} : InputBoxProps) {
 
   const buttonClass = buttonStatus ? 'input-primary-button' : 'input-disable-button';
   const messageClass = 'input-message ' + (error ? 'error' : 'primary'); 
@@ -23,7 +24,7 @@ export default function InputBox({label, type, value, placeholder, onChangeHandl
     <div className = "input-box">
       <div className = "input-label label">{label}</div>
       <div className = "input-content-box">
-        <input className = "input" type={type} value= {value} placeholder={placeholder} onChange={onChangeHandler}/>
+        <input className = "input" type={type} value= {value} placeholder={placeholder} onChange={onChangeHandler} onKeyDown={onKeydownHandler}/>
         {/* && 연산자를 사용해 buttonTitle이 있으면 div 요소를 표시 아니면 표시 안함*/}
         {buttonTitle && <div className = {buttonClass} onClick={onButtonClickHandler}>{buttonTitle}</div>} 
       </div>
