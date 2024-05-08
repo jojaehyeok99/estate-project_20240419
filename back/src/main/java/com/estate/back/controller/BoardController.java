5267413a268ffe1e90fba0f845c6eb472756a6e7
jojaehyeok99 <choco5366@naver.com>
2024-05-08 14:22:24 +0900

    2024 - 05 - 08 프로젝트 :  백엔드 - Q&A 게시물 수정 API 구현

--- back/src/main/java/com/estate/back/controller/BoardController.java
+++ back/src/main/java/com/estate/back/controller/BoardController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estate.back.dto.request.board.PostBoardRequestDto;
 import com.estate.back.dto.request.board.PostCommentRequestDto;
+import com.estate.back.dto.request.board.PutBoardRequestDto;
 import com.estate.back.dto.response.ResponseDto;
 import com.estate.back.dto.response.board.GetBoardListResponseDto;
 import com.estate.back.dto.response.board.GetBoardResponseDto;
@@ -65,6 +67,16 @@ public class BoardController {
     @PathVariable("receptionNumber") int receptionNumber
   ) {
     ResponseEntity<? super GetBoardResponseDto> response = boardService.getBoard(receptionNumber);
+    return response;
+  }
+
+  @PutMapping("/{receptionNumber}")
+  public ResponseEntity<ResponseDto> putBoard (
+    @RequestBody @Valid PutBoardRequestDto requestBody,
+    @PathVariable("receptionNumber") int receptionNumber,
+    @AuthenticationPrincipal String userId
+  ) {
+    ResponseEntity<ResponseDto> response = boardService.putBoard(requestBody, receptionNumber, userId);
     return response;
   }
 
--- back/src/main/java/com/estate/back/service/BoardService.java
+++ back/src/main/java/com/estate/back/service/BoardService.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 
 import com.estate.back.dto.request.board.PostBoardRequestDto;
 import com.estate.back.dto.request.board.PostCommentRequestDto;
+import com.estate.back.dto.request.board.PutBoardRequestDto;
 import com.estate.back.dto.response.ResponseDto;
 import com.estate.back.dto.response.board.GetBoardListResponseDto;
 import com.estate.back.dto.response.board.GetBoardResponseDto;
@@ -16,6 +17,8 @@ public interface BoardService {
   ResponseEntity<? super GetBoardListResponseDto> getBoardList();
   ResponseEntity<? super GetSearchBoardListResponseDto> getSearchBoardList(String searchWord);
   ResponseEntity<? super GetBoardResponseDto> getBoard(int receptionNumber);
+
+  ResponseEntity<ResponseDto> putBoard(PutBoardRequestDto dto, int receptionNumber, String userId);
 
   ResponseEntity<ResponseDto> increaseViewCount(int receptionNumber);
 
