a795d60b1b85b5116234232553489b3cb0bd1ffa
jojaehyeok99 <choco5366@naver.com>
2024-05-09 10:52:20 +0900

    2024 - 05 - 09 프로젝트 : 프론트 - 지역평균 페이지 만들기

---
+++ front/src/components/Selectbox/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css';
+
+export default function SelectBox() {
+  return (
+    <div className='select-box'>
+      <div className='select-selected'></div>
+      <div className='select-open-button'></div>
+    </div>
+  );
+}
