bebcb7647a3d9f3f9802c25ef9a68cfaf2acfb7c
jojaehyeok99 <choco5366@naver.com>
2024-05-10 10:50:58 +0900

    2024 - 05 - 10 프로젝트 : 백엔드 - 지역 평균 데이터 불러오기 API 구현(2)

--- back/src/main/java/com/estate/back/dto/response/estate/GetLocalDataResponseDto.java
+++ back/src/main/java/com/estate/back/dto/response/estate/GetLocalDataResponseDto.java
@@ -1,15 +1,16 @@
 package com.estate.back.dto.response.estate;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
-
+import com.estate.back.common.util.ChangeDateFormatUtil;
 import com.estate.back.dto.response.ResponseCode;
 import com.estate.back.dto.response.ResponseDto;
 import com.estate.back.dto.response.ResponseMessage;
+import com.estate.back.repository.resultSet.GetLocalDataResultSet;
 
 import lombok.Getter;
 
@@ -20,16 +21,31 @@ public class GetLocalDataResponseDto extends ResponseDto{
   private List<Integer> lease;
   private List<Integer> monthRent;
 
-  private GetLocalDataResponseDto () {
+  private GetLocalDataResponseDto (List<GetLocalDataResultSet> resultSets) throws Exception{
     super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
-    this.yearMonth = null;
-    this.sale = null;
-    this.lease = null;
-    this.monthRent = null;
+
+    List<String> yearMonth = new ArrayList<>();
+    List<Integer> sale = new ArrayList<>();
+    List<Integer> lease = new ArrayList<>();
+    List<Integer> monthRent = new ArrayList<>();
+
+    for (GetLocalDataResultSet resultSet: resultSets) {
+      String originalDate = resultSet.getYearMonth();
+      yearMonth.add(ChangeDateFormatUtil.changeYYMM(originalDate));
+      sale.add(resultSet.getSale());
+      lease.add(resultSet.getLease());
+      monthRent.add(resultSet.getMonthRent());
+    }
+
+    this.yearMonth = yearMonth;
+    this.sale = sale;
+    this.lease = lease;
+    this.monthRent = monthRent;
   }
 
-  public static ResponseEntity<GetLocalDataResponseDto> success (String data) {
-    GetLocalDataResponseDto responseBody = new GetLocalDataResponseDto();
+  public static ResponseEntity<GetLocalDataResponseDto> success (List<GetLocalDataResultSet> resultSets) throws Exception{
+    GetLocalDataResponseDto responseBody = new GetLocalDataResponseDto(resultSets);
     return ResponseEntity.status(HttpStatus.OK).body(responseBody);
   }
+
 }
--- back/src/main/java/com/estate/back/service/implementation/EstateServiceImplementation.java
+++ back/src/main/java/com/estate/back/service/implementation/EstateServiceImplementation.java
@@ -1,11 +1,14 @@
 package com.estate.back.service.implementation;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.estate.back.dto.response.ResponseDto;
 import com.estate.back.dto.response.estate.GetLocalDataResponseDto;
 import com.estate.back.repository.EstateRepository;
+import com.estate.back.repository.resultSet.GetLocalDataResultSet;
 import com.estate.back.service.EstateService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,16 +23,13 @@ public class EstateServiceImplementation implements EstateService{
   public ResponseEntity<? super GetLocalDataResponseDto> getLocalData(String local) {
     try{
 
-      
-
-
-
-
+      List<GetLocalDataResultSet> resultSets = estateRepository.getLocalData(local);
+      return GetLocalDataResponseDto.success(resultSets);
 
     } catch (Exception exception) {
       exception.printStackTrace();
       return ResponseDto.databaseError();
     }
-    return GetLocalDataResponseDto.success(null);
+    
   }
 }
